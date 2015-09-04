96fd7405e2eb8cca5364154d43096fdc2c82fc21
Adam Cantrowitz <cantrowitz@gmail.com>
2015-09-04 00:01:29 -0400

    added java docs

--- rxbroadcast/src/main/java/com/cantrowitz/rxbroadcast/RxBroadcast.java
+++ rxbroadcast/src/main/java/com/cantrowitz/rxbroadcast/RxBroadcast.java
@@ -1,5 +1,6 @@
 package com.cantrowitz.rxbroadcast;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
@@ -13,16 +14,42 @@ import rx.Observable;
  */
 public class RxBroadcast {
 
+    private RxBroadcast() {
+        throw new AssertionError("No instances");
+    }
+
+    /**
+     * Create {@link Observable} that wraps {@link BroadcastReceiver} and emits received intents.
+     *
+     * @param context the context the {@link BroadcastReceiver} will be created from
+     * @param intentFilter the filter for the particular intent
+     * @return Observable of {@link Intent} that matches the filter
+     */
     public static Observable<Intent> fromBroadcast(Context context, IntentFilter intentFilter) {
         return Observable.create(new GlobalBroadcastProvider(intentFilter, context));
     }
 
+    /**
+     *  Create {@link Observable} that wraps {@link BroadcastReceiver} and emits received intents.
+     * @param context the context the {@link BroadcastReceiver} will be created from
+     * @param intentFilter the filter for the particular intent
+     * @param broadcastPermission String naming a permissions that a broadcaster must hold in order to send an Intent to you. If null, no permission is required.
+     * @param handler Handler identifying the thread that will receive the Intent. If null, the main thread of the process will be used.
+     * @return
+     */
     public static Observable<Intent> fromBroadcast(Context context, IntentFilter intentFilter,
                                                    String broadcastPermission, Handler handler) {
         return Observable.create(new GlobalWPermissionsBroadcastProvider(intentFilter, context,
                 broadcastPermission, handler));
     }
-
+    /**
+     * Create {@link Observable} that wraps {@link BroadcastReceiver} and emits received intents.
+     * <p>
+     *  This uses a {@link LocalBroadcastManager}
+     * @param context the context the {@link BroadcastReceiver} will be created from
+     * @param intentFilter the filter for the particular intent
+     * @return Observable of {@link Intent} that matches the filter
+     */
     public static Observable<Intent> fromLocalBroadcast(Context context, IntentFilter intentFilter) {
         return Observable.create(new LocalBroadcastProvider(intentFilter,
                 LocalBroadcastManager.getInstance(context)));
